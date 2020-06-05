540afce2210bdbe256a20d32c6308c7f803ce6c4
Afrar Malakooth <mmafrar@gmail.com>
2020-06-05 14:24:49 +0530

    Integrated WKWebView to create a simple browser

--- wkwebview/ViewController.swift
+++ wkwebview/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://www.google.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
 

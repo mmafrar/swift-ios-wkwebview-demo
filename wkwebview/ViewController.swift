35147ccb0ac1bab1c8a0b3d0615726c0be21ded8
Afrar Malakooth <mmafrar@gmail.com>
2020-06-05 13:34:09 +0530

    Setting up the development environment

---
+++ wkwebview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wkwebview
+//
+//  Created by Afrar Malakooth on 6/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
